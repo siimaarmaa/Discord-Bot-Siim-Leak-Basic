17a73251dbdfa90fe2ab2adb279e8c904084ddd4
Siim Aarmaa <51194757+siimaarmaa@users.noreply.github.com>
2023-12-08 20:01:33 +0200

    code update

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,59 +1,46 @@
 import nextcord
 from nextcord.ext import commands
+from nextcord.ext.tasks import loop
+from nextcord.utils import get
+from typing import List
+import datetime
+import time
 
 
 class ServerStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # Define the channels where the stats will be displayed
+        self.channels = [1182060683014189116]
+
+        # Create a task that will run every 60 seconds (1 minute)
+        self.update_stats_task = self.bot.loop.create_task(self.update_stats())
+
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            return  # Ignore messages from bots
+    async def on_ready(self):
+        print('Server Stats cog is ready')
 
-        if "show stats" in message.content.lower():
-            await self.send_server_stats(message.channel)
+    async def update_stats(self):
+        # Get the current server stats
+        stats = await self.bot.fetch_server_stats()
 
-    async def send_server_stats(self, channel):
-        guild = channel.guild
+        # Format the stats into a string
+        formatted_stats = f'''
+            **Server Stats**
 
-        # Get general server statistics
-        total_members = guild.member_count
-        online_members = sum(member.status != nextcord.Status.offline for member in guild.members)
-        text_channels = len(guild.text_channels)
-        voice_channels = len(guild.voice_channels)
-        bot_members = sum(member.bot for member in guild.members)
+            **Total Members:** {stats.member_count}
+            **Online Members:** {stats.online_member_count}
+            **Server Created:** {datetime.datetime.fromtimestamp(stats.created_at).strftime('%Y-%m-%d %H:%M:%S')}
+            **Server Activity:** {stats.activity_count}
+        '''
 
-        # Get voice chat information
-        voice_channels_info = {}
+        # Send the formatted stats to all the channels
+        for channel in self.channels:
+            await self.bot.get_channel(channel).send(formatted_stats)
 
-        for voice_channel in guild.voice_channels:
-            category_name = voice_channel.category.name if voice_channel.category else "Uncategorized"
-
-            if category_name not in voice_channels_info:
-                voice_channels_info[category_name] = []
-
-            members_in_channel = len(voice_channel.members)
-            bots_in_channel = sum(member.bot for member in voice_channel.members)
-            voice_channels_info[category_name].append(
-                f"{voice_channel.name}: {members_in_channel} members ({bots_in_channel} bots)"
-            )
-
-        # Send the information to the channel
-        info_message = f"Server Statistics for {guild.name} (All-time):\n" \
-                       f"Total Members: {total_members}\n" \
-                       f"Online Members: {online_members}\n" \
-                       f"Total Bots: {bot_members}\n" \
-                       f"Text Channels: {text_channels}\n" \
-                       f"Voice Channels: {voice_channels}\n\n" \
-                       "Voice Channel Information:\n"
-
-        for category_name, channels_info in voice_channels_info.items():
-            info_message += f"\nCategory: {category_name}\n"
-            info_message += "\n".join(channels_info)
-            info_message += "\n"
-
-        await channel.send(info_message)
+        # Delay the task by 60 seconds (1 minute)
+        await asyncio.sleep(60)
 
 
 def setup(bot):
