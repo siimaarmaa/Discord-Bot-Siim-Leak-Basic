f141ad8b5f2ef9281789a6e05eebf97dee665624
Siim Aarmaa <51194757+siimaarmaa@users.noreply.github.com>
2023-12-08 19:57:20 +0200

    code update

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,15 +1,21 @@
 import nextcord
 from nextcord.ext import commands
-from nextcord import slash_command
 
 
 class ServerStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name='serverstats', description='ServerStats', guild_ids=[])
-    async def server_stats(self, ctx):
-        guild = ctx.guild
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return  # Ignore messages from bots
+
+        if "show stats" in message.content.lower():
+            await self.send_server_stats(message.channel)
+
+    async def send_server_stats(self, channel):
+        guild = channel.guild
 
         # Get general server statistics
         total_members = guild.member_count
@@ -19,18 +25,35 @@ class ServerStats(commands.Cog):
         bot_members = sum(member.bot for member in guild.members)
 
         # Get voice chat information
-        voice_channels_info = []
+        voice_channels_info = {}
+
         for voice_channel in guild.voice_channels:
+            category_name = voice_channel.category.name if voice_channel.category else "Uncategorized"
+
+            if category_name not in voice_channels_info:
+                voice_channels_info[category_name] = []
+
             members_in_channel = len(voice_channel.members)
-            voice_channels_info.append(f"{voice_channel.name}: {members_in_channel} members")
+            bots_in_channel = sum(member.bot for member in voice_channel.members)
+            voice_channels_info[category_name].append(
+                f"{voice_channel.name}: {members_in_channel} members ({bots_in_channel} bots)"
+            )
 
         # Send the information to the channel
-        await ctx.send(f"Server Statistics for {guild.name}:\n"
-                       f"Total Members: {total_members}\n"
-                       f"Online Members: {online_members}\n"
-                       f"Total Bots: {bot_members}\n"
-                       f"Text Channels: {text_channels}\n"
-                       f"Voice Channels: {voice_channels}\n\n")
+        info_message = f"Server Statistics for {guild.name} (All-time):\n" \
+                       f"Total Members: {total_members}\n" \
+                       f"Online Members: {online_members}\n" \
+                       f"Total Bots: {bot_members}\n" \
+                       f"Text Channels: {text_channels}\n" \
+                       f"Voice Channels: {voice_channels}\n\n" \
+                       "Voice Channel Information:\n"
+
+        for category_name, channels_info in voice_channels_info.items():
+            info_message += f"\nCategory: {category_name}\n"
+            info_message += "\n".join(channels_info)
+            info_message += "\n"
+
+        await channel.send(info_message)
 
 
 def setup(bot):
