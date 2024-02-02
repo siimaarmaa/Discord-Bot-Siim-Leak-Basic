625e955d47d0ea3959bfda96fb9cea3cbbe0290a
siimaarmaa <siim.aarmaa@hotmail.com>
2024-02-02 23:05:46 +0200

    channels changes

--- cogs/randomjoke.py
+++ cogs/randomjoke.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from nextcord import Interaction, slash_command
+import nextcord
 from nextcord.ext import commands
 
 
@@ -8,17 +8,14 @@ class RandomJoke(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name='joke', description='Random Joke', guild_ids=[])
-    async def joke(
-            self,
-            ctx: Interaction
-    ):
+    @nextcord.slash_command(name="joke", description="Random Joke")
+    async def joke(self, interaction: nextcord.Interaction):
         joke_url = 'https://icanhazdadjoke.com/'
         headers = {
             'Accept': 'application/json'
         }
         joke_chat = requests.request('get', joke_url, headers=headers)
-        await ctx.response.send_message(json.loads(joke_chat.text)['joke'])
+        await interaction.send(json.loads(joke_chat.text)['joke'])
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -2,22 +2,26 @@
 # Author: Siim "Siim Leaks" Aarmaa - www.aarmaa.ee
 # Facebook: Aarmaa - https://www.facebook.com/aarmaa.ee
 # Start year: 17.08.2021
-# Version number: v.0.2.42
-# Last update: 01.02.2024
+# Version number: v.0.2.43
+# Last update: 02.02.2024
 
 import nextcord
 import os
 from dotenv import load_dotenv
 from nextcord.ext import commands
 
-# Secrets set up
+# Load environment variables
 load_dotenv()
 token = os.getenv('TOKEN')
-guild = os.getenv('GUILD')
+guild_name = os.getenv('GUILD')  # Renamed to avoid confusion with the guild object in on_ready
+
+# Loading channel IDs from environment variables
+activity_logs_channel_id = int(os.getenv('ACTIVITY_LOGS_CHANNEL_ID'))  # Added environment variable
+message_delete_channel_id = int(os.getenv('MESSAGE_DELETE_CHANNEL_ID'))  # Added environment variable
+message_edit_channel_id = int(os.getenv('MESSAGE_EDIT_CHANNEL_ID'))  # Added environment variable
 
 # Discord bot code start
-# Command prefix
-# Gateway intents
+# Setting up Discord bot with intents
 intents = nextcord.Intents.all()
 bot = commands.Bot(intents=intents)
 
@@ -25,54 +29,53 @@ bot = commands.Bot(intents=intents)
 # Bot Status Message
 @bot.event
 async def on_ready():
-    global guild
+    global guild_name  # Reference the global variable
     await bot.change_presence(status=nextcord.Status.online, activity=nextcord.Game('I was made in Python!!'))
 
     for guild in bot.guilds:
-        if guild.name == guild:
+        if guild.name == guild_name:  # Compare with the correct guild name
+            print(f'{bot.user} is connected to the guild: {guild.name}(id: {guild.id})')
             break
-
-    print(
-        f'{bot.user} is connected to following guild: \n'
-        f'{guild.name}(id: {guild.id})'
-    )
 
 
 # Leaver log
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1202606214215766067)  # activity-logs
-    await channel.send(f"{member.name} is leaver. Don't say goodbye!!")
+    channel = bot.get_channel(activity_logs_channel_id)
+    if channel:
+        await channel.send(f"{member.name} has left. Don't say goodbye!!")
 
 
 # Logs for mods and admins
 @bot.event
 async def on_message_delete(message):
-    embed = nextcord.Embed(title=f'{message.author.name} has deleted a message | {message.author.id}',
-                           description=f'{message.content}')
-    channel = bot.get_channel(1202606325767479297)  # activity-logs
-    await channel.send(embed=embed)
+    channel = bot.get_channel(message_delete_channel_id)
+    if channel:
+        embed = nextcord.Embed(title=f'{message.author.name} has deleted a message | {message.author.id}',
+                               description=f'{message.content}')
+        await channel.send(embed=embed)
 
 
 @bot.event
 async def on_message_edit(message_before, message_after):
-    embed = nextcord.Embed(title=f'{message_before.author.name} has edited a message | {message_before.author.id}')
-    embed.add_field(name='Before Message', value=f'{message_before.content}', inline=False)
-    embed.add_field(name='After Message', value=f'{message_after.content}', inline=False)
-    channel = bot.get_channel(1202606441253437501)  # activity-logs
-    await channel.send(embed=embed)
+    channel = bot.get_channel(message_edit_channel_id)
+    if channel:
+        embed = nextcord.Embed(title=f'{message_before.author.name} has edited a message | {message_before.author.id}')
+        embed.add_field(name='Before Message', value=message_before.content, inline=False)
+        embed.add_field(name='After Message', value=message_after.content, inline=False)
+        await channel.send(embed=embed)
 
 
 # Cog start
-bot.load_extension('cogs.moderated')  # Admins and Moderators commands
-bot.load_extension('cogs.dogpic')  # Random Dog picture
-bot.load_extension('cogs.ping')  # Test bot ping
-bot.load_extension('cogs.randomjoke')  # Random Joke
-bot.load_extension('cogs.reactrole')  # Self role react for fun
-bot.load_extension('cogs.support')  # Siim Leaks Basic Support
-bot.load_extension('cogs.help')  # Bot commands help menu
-bot.load_extension('cogs.report')  # Report user and message
-bot.load_extension('cogs.ticket')  # Create bug ticket or problem ticket
+# Loading cogs
+cog_extensions = ['cogs.moderated', 'cogs.dogpic', 'cogs.ping', 'cogs.randomjoke', 'cogs.reactrole',
+                  'cogs.support', 'cogs.help', 'cogs.report', 'cogs.ticket']
+
+for extension in cog_extensions:
+    try:
+        bot.load_extension(extension)
+    except Exception as e:
+        print(f"Failed to load extension {extension}: {e}")
 # Cog end
 
 # Discord bot code end
